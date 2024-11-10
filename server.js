de70dd33fb17a450e5eeec6239256540e3b15e82
Deepak8218 <deepakprince917@gmail.com>
2024-11-10 13:59:33 +0530

    Updated server.js with new changes

--- server.js
+++ server.js
@@ -10,9 +10,20 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // MongoDB connection (without deprecated options)
-mongoose.connect('mongodb://localhost:27017/portfolio')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log('Error connecting to MongoDB:', err));
+
+
+
+
+const mongoURI = 'mongodb+srv://darkweb8218:lfUiDalbYWJitb3d@cluster0.lou8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(mongoURI)
+  .then(() => console.log('Connected to MongoDB Atlas'))
+  .catch(err => console.error('Error connecting to MongoDB Atlas:', err));
+
+
+// mongoose.connect('mongodb://localhost:27017/portfolio')
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch(err => console.log('Error connecting to MongoDB:', err));
 
 // Schema and Model for form data
 const formSchema = new mongoose.Schema({
@@ -50,3 +61,19 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// darkweb8218
+// lfUiDalbYWJitb3d
